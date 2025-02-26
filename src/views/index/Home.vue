c9690d8c3b593056d9839e484a5eaf157f2457ce
uedarap <uedarap@gmail.com>
2025-02-25 22:28:32 -0300

    site ok ✅

--- src/views/index/Home.vue
+++ src/views/index/Home.vue
@@ -61,7 +61,7 @@ async function enviarFormulario() {
       <div class="inner">
         <div class="logo">
           <!-- <span class="icon fa-gem"></span> -->
-          <img src="./assets/images/htl.png" alt="" style="width: 300px;">
+          <img src="../../assets/images/htl.png" alt="" style="width: 300px;">
         </div>
         <h2 style="font-weight: 100; font-size: 25pt; text-transform: uppercase;">Engenharia e Serviços</h2>
         <!-- <p>Another free + fully responsive site template by <a href="http://html5up.net">HTML5 UP</a></p> -->
@@ -74,7 +74,7 @@ async function enviarFormulario() {
       <!-- One -->
       <section id="one" class="wrapper spotlight style1">
         <div class="inner">
-          <a href="#" class="image"><img src="./assets/images/pic01.jpg" alt="" /></a>
+          <a href="#" class="image"><img src="../../assets/images/pic01.jpg" alt="" /></a>
           <div class="content">
             <h2 class="major">Magna arcu feugiat</h2>
             <p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non,
@@ -88,7 +88,7 @@ async function enviarFormulario() {
       <!-- Two -->
       <section id="two" class="wrapper alt spotlight style2">
         <div class="inner">
-          <a href="#" class="image"><img src="./assets/images/pic02.jpg" alt="" /></a>
+          <a href="#" class="image"><img src="../../assets/images/pic02.jpg" alt="" /></a>
           <div class="content">
             <h2 class="major">Tempus adipiscing</h2>
             <p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non,
@@ -102,7 +102,7 @@ async function enviarFormulario() {
       <!-- Three -->
       <section id="three" class="wrapper spotlight style3">
         <div class="inner">
-          <a href="#" class="image"><img src="./assets/images/pic03.jpg" alt="" /></a>
+          <a href="#" class="image"><img src="../../assets/images/pic03.jpg" alt="" /></a>
           <div class="content">
             <h2 class="major">Nullam dignissim</h2>
             <p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non,
@@ -122,25 +122,25 @@ async function enviarFormulario() {
             Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
           <section class="features">
             <article>
-              <a href="#" class="image"><img src="./assets/images/pic04.jpg" alt="" /></a>
+              <a href="#" class="image"><img src="../../assets/images/pic04.jpg" alt="" /></a>
               <h3 class="major">Sed feugiat lorem</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
               <a href="#" class="special">Learn more</a>
             </article>
             <article>
-              <a href="#" class="image"><img src="./assets/images/pic05.jpg" alt="" /></a>
+              <a href="#" class="image"><img src="../../assets/images/pic05.jpg" alt="" /></a>
               <h3 class="major">Nisl placerat</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
               <a href="#" class="special">Learn more</a>
             </article>
             <article>
-              <a href="#" class="image"><img src="./assets/images/pic06.jpg" alt="" /></a>
+              <a href="#" class="image"><img src="../../assets/images/pic06.jpg" alt="" /></a>
               <h3 class="major">Ante fermentum</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
               <a href="#" class="special">Learn more</a>
             </article>
             <article>
-              <a href="#" class="image"><img src="./assets/images/pic07.jpg" alt="" /></a>
+              <a href="#" class="image"><img src="../../assets/images/pic07.jpg" alt="" /></a>
               <h3 class="major">Fusce consequat</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.</p>
               <a href="#" class="special">Learn more</a>
@@ -204,5 +204,5 @@ async function enviarFormulario() {
 </template>
 
 <style lang="scss">
-@import './style.scss';
+  @use '../../style.scss';
 </style>
