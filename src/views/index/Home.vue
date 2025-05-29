9ed1350c2b550173f5562a85d5ff638d7c5176b1
uedarap <uedarap@gmail.com>
2025-05-29 12:28:06 -0300

    dados da empresa e produtos ✅

--- src/views/index/Home.vue
+++ src/views/index/Home.vue
@@ -85,47 +85,44 @@ const knobValueChange = (value: number) => {
       <!-- One -->
       <section id="one" class="wrapper spotlight style1 opacity">
         <div class="inner">
-          <a href="#" class="image"><img src="../../assets/images/pic01.jpg" alt="" /></a>
+          <a href="#" class="image"><img src="../../assets/images/brain.png" alt="" style="mix-blend-mode: color-dodge; opacity: .8;"/></a>
           <div class="content">
-            <h2 class="major">Magna arcu feugiat</h2>
-            <p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non,
-              sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam
-              ultrices, neque et faucibus viverra, ex nulla cursus.</p>
-            <a href="#" class="special">Learn more</a>
+            <h2 class="major">Iluminação inteligente</h2>
+            <p>A iluminação das cidades mudou muito nos últimos anos, saindo de lâmpadas simples e gastonas para LEDs modernos e, agora, sistemas inteligentes. A ideia é usar sensores e redes sem fio para ajustar a luz automaticamente, de acordo com a necessidade do momento, como trânsito ou clima ruim. Isso não só economiza até 70% de energia, mas também deixa as ruas mais seguras e os espaços urbanos mais eficientes e sustentáveis. É uma forma prática de combinar tecnologia e sustentabilidade no dia a dia das cidades.</p>
+            <!-- <a href="#" class="special">Learn more</a> -->
           </div>
         </div>
       </section>
 
       <!-- Two -->
-      <section id="two" class="wrapper alt spotlight style2">
+      <section id="two" class="wrapper alt spotlight style2 opacity">
         <div class="inner">
-          <a href="#" class="image"><img src="../../assets/images/pic02.jpg" alt="" /></a>
+          <h2 class="major">Nossas soluções</h2>
+        </div>
+        <div class="inner">
+          <a href="#" class="image"><img src="../../assets/images/eagle.png" alt="" /></a>
           <div class="content">
-            <h2 class="major">Tempus adipiscing</h2>
-            <p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non,
-              sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam
-              ultrices, neque et faucibus viverra, ex nulla cursus.</p>
-            <a href="#" class="special">Learn more</a>
+            <h2 class="major">Eagle Eye 3.0</h2>
+            <p>O Eagle Eye é um sistema de iluminação pública inteligente com detecção por radar, que aciona luzes automaticamente ao detectar movimento e reduz a intensidade quando não há atividade, economizando energia. Ele usa comunicação por rádio para conectar controladores e garantir iluminação eficiente e segura para pedestres, ciclistas e veículos, contribuindo para a preservação ambiental.</p>
+            <a href="#" class="special">Saiba mais sobre esse produto</a>
           </div>
         </div>
       </section>
 
       <!-- Three -->
-      <section id="three" class="wrapper spotlight style3">
+      <section id="three" class="wrapper spotlight style3 opacity">
         <div class="inner">
-          <a href="#" class="image"><img src="../../assets/images/pic03.jpg" alt="" /></a>
+          <a href="#" class="image"><img src="../../assets/images/zhaga.png" alt="" /></a>
           <div class="content">
-            <h2 class="major">Nullam dignissim</h2>
-            <p>Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non,
-              sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam
-              ultrices, neque et faucibus viverra, ex nulla cursus.</p>
-            <a href="#" class="special">Learn more</a>
+            <h2 class="major">Eagle Eye Zhaga</h2>
+            <p>O Eagle Eye Zhaga é um controlador de luz compatível com os padrões <b>Zhaga Book 18 e Dali-2</b>, capaz de detectar pedestres, ciclistas e veículos por meio de um sensor de movimento com radar Doppler. Ele ilumina o caminho à frente e pode atuar como controlador principal ou auxiliar em redes integradas. Seu compartimento ajustável permite adaptar a área de detecção para diferentes necessidades.</p>
+            <a href="#" class="special">Saiba mais sobre esse produto</a>
           </div>
         </div>
       </section>
 
       <!-- Four -->
-      <section id="four" class="wrapper alt style1">
+      <section id="four" class="wrapper alt style1 opacity">
         <div class="inner">
           <h2 class="major">Vitae phasellus</h2>
           <p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce
